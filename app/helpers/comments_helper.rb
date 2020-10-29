432ad4770020cd1321ddd0c64b82c3fd148eaa87
Sergio <sergioobolevich@gmail.com>
2020-10-29 13:14:06 -0300

    finished blog comments feature

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -1,5 +1,5 @@
 module CommentsHelper
   def comment_params
-    params.require(:comment).permit(:author_name, :body, :article_id)
+    params.require(:comment).permit(:author_name, :body)
   end
 end
